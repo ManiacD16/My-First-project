272d9507d88da1a6fc4e1ccc6c185cc879b09f35
ManiacD16 <msd161918@gmail.com>
2024-02-28 18:13:26 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+// alert("Hello");
+
+// $(document).ready(function () {
+//     $('#myButton').click(function (e) {
+//         e.preventDefault();
+
+//         var name = $('#name').val();
+//         var email = $('#email').val();
+//         var password = $('#password').val();
+//         alert("Hi " + name);
+//         alert("Your Password is- " + name);
+//         alert("And Your Email is- " + email);
+
+//         location.href = 'login.html';
+//     });
+// });
